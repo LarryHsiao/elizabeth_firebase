dcc7f977ba62b8915fed32480a0a84b787654b9e
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-05-25 01:19:38 +0800

    skeleton for checking subs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,68 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as express from 'express';
+import * as key from '../key.json';
+import {google} from 'googleapis';
+import {CallableContext} from 'firebase-functions/lib/providers/https';
 
 require("firebase/firestore");
-admin.initializeApp(functions.config().firebase)
 
-const app = express();
-// const db = admin.firestore();
-app.get('/jots', (req, res) => {
-    // db.collection("q41FpvsIiYV9K241Jmuc7Lt71jN2/data/jots")
-    //     .orderBy('createdTime', 'desc')
-    //     .get()
-    //     .then((query) => {
-    //         query.forEach((item) => {
-    //             res.send(item.data().content)
-    //         });
-    //     })
-    //     .catch((err) => {
-    //         console.log("this is err: " + err)
-    //     });
-})
+admin.initializeApp()
+let firestore = admin.firestore();
 
-exports.app = functions.https.onRequest(app);
+const authClient = new google.auth.JWT({
+    email: key.client_email,
+    key: key.private_key,
+    scopes: ["https://www.googleapis.com/auth/androidpublisher"]
+});
 
+const playDeveloperApiClient = google.androidpublisher({
+    version: 'v3',
+    auth: authClient
+});
+
+exports.subVerification = functions.https.onCall(async (data, context: CallableContext) => {
+    try {
+        await authClient.authorize();
+        const subscription = await playDeveloperApiClient.purchases.subscriptions.get({
+            packageName: data.package_name,
+            subscriptionId: data.sku_id,
+            token: data.purchase_token
+        });
+        if (subscription.status === 200) {
+            return {
+                status: 200,
+                message: "Subscription verification successfully!"
+            }
+        } else {
+            return {
+                status: 500,
+                message: "Failed to verify subscription, Try again!"
+            }
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            status: 500,
+            message: "Failed to verify subscription, Try again!"
+        }
+    }
+});
+
+// exports.checkSubs = functions.pubsub.schedule('0/1 0 * * *').onRun((context) => {
+exports.checkSubs = functions.https.onRequest(async (req, res) => {
+    firestore.collection("q41FpvsIiYV9K241Jmuc7Lt71jN2/data/jots")
+        .orderBy('createdTime', 'desc')
+        .get()
+        .then((query) => {
+            query.forEach((item) => {
+                console.log(item.data())
+                // res.send(item.data().content);
+            });
+            res.send("abc");
+        })
+        .catch((err) => {
+                console.log("this is err: " + err)
+            }
+        );
+    return null;
+});
