1494f15ad76e775fc439386470ad6026228f6d56
Larry Hsiao <larryhsiao@larryhsiao.com>
2020-06-01 00:49:36 +0800

    daily check

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,13 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as key from '../key.json';
-import {google} from 'googleapis';
+import {androidpublisher_v3, google} from 'googleapis';
 
 require("firebase/firestore");
 
 admin.initializeApp()
-let firestore = admin.firestore();
+const firestore = admin.firestore();
+const storage = admin.storage();
 
 const authClient = new google.auth.JWT({
     email: key.client_email,
@@ -27,13 +28,76 @@ export interface Subscription {
     changeUser: boolean
 }
 
+exports.dailySubCheck = functions.pubsub.schedule('0 0 * * *').onRun((() => {
+    return subCheck();
+}));
+
+async function subCheck() {
+    const currentTime = new Date().getTime();
+    const purchasesRef = firestore.collection("purchases");
+    const expires = await purchasesRef.where("expiryTimeMillis", "<", currentTime).get();
+    await authClient.authorize();
+    // noinspection ES6MissingAwait
+    expires.forEach(async (expired) => {
+        const expiredData = expired.data();
+        const playRes = await playDeveloperApiClient.purchases.subscriptions.get({
+            packageName: expiredData.packageName,
+            subscriptionId: expiredData.productId,
+            token: expired.id
+        });
+        if (playRes.status == 200) {
+            if (playRes.data.orderId !== expired.data().orderId) {
+                if (Number(playRes.data.expiryTimeMillis) > currentTime) {
+                    await updatePurchaseInfo(
+                        firestore.doc(`purchases/${expired.id}`),
+                        playRes.data,
+                        new class implements Subscription {
+                            changeUser = false
+                            package_name = expiredData.packageName
+                            purchase_token = expired.id
+                            sku_id = expiredData.productId
+                            uid = expiredData.uid
+                        }
+                    )
+                    await updatePurchaseInfo(
+                        firestore.doc(`${expiredData.uid}/account/purchases/${expired.id}`),
+                        playRes.data,
+                        new class implements Subscription {
+                            changeUser = false
+                            package_name = expiredData.packageName
+                            purchase_token = expired.id
+                            sku_id = expiredData.productId
+                            uid = expiredData.uid
+                        }
+                    )
+                    await updatePremiumState(expiredData.uid)
+                    return;
+                }
+            }
+        }
+        if (expiredData.expiryTimeMillis + 2592000000  /* 30days */ < currentTime) {
+            await firestore.doc(`purchases/${expired.id}`).delete()
+            await firestore.doc(`${expiredData.uid}/account/purchases/${expired.id}`).delete()
+            const purchases = await firestore.collection(`${expiredData.uid}/account/purchases/`).listDocuments();
+            if (purchases.length == 0) {
+                await deleteCollection(firestore, `${expiredData.uid}/data/attachments`, 10);
+                await deleteCollection(firestore, `${expiredData.uid}/data/jots`, 10);
+                await deleteCollection(firestore, `${expiredData.uid}/data/tag_jot`, 10);
+                await deleteCollection(firestore, `${expiredData.uid}/data/tags`, 10);
+                await storage.bucket().deleteFiles({prefix: `${expiredData.uid}/`})
+            }
+        }
+        await updatePremiumState(expiredData.uid);
+    })
+}
+
 /**
  * Use to check the subscription status. Return error to inform user to change account for cloud functions.
  */
 exports.subscribtion = functions.https.onRequest(async (req, res) => {
     try {
-        const subReq = req.body as Subscription
-        if (subReq.uid == undefined) {
+        const sub = req.body as Subscription
+        if (sub.uid == undefined) {
             res.status(400)
             res.send({
                 message: "Required UID"
@@ -42,30 +106,28 @@ exports.subscribtion = functions.https.onRequest(async (req, res) => {
         }
         await authClient.authorize();
         const playRes = await playDeveloperApiClient.purchases.subscriptions.get({
-            packageName: subReq.package_name,
-            subscriptionId: subReq.sku_id,
-            token: subReq.purchase_token
+            packageName: sub.package_name,
+            subscriptionId: sub.sku_id,
+            token: sub.purchase_token
         });
         if (playRes.status === 200) {
-            const adminOrderRef = firestore.doc(`purchases/${subReq.purchase_token}`);
+            const adminOrderRef = firestore.doc(`purchases/${sub.purchase_token}`);
             const order = await adminOrderRef.get();
             if (order.exists) {
                 const existUid = await order.get("uid");
-                if (existUid == subReq.uid) {
+                if (existUid == sub.uid) {
                     res.sendStatus(204)
                 } else {
-                    if (subReq.changeUser) {
-                        await firestore.doc(`${existUid}/account/purchases/${subReq.purchase_token}`).delete();
+                    if (sub.changeUser) {
+                        await firestore.doc(`${existUid}/account/purchases/${sub.purchase_token}`).delete();
                         await updatePremiumState(existUid);
-                        const userOrderRef = firestore.doc(`${subReq.uid}/account/purchases/${subReq.purchase_token}`);
-                        await userOrderRef.set(playRes.data)
-                        await userOrderRef.update({
-                            uid: subReq.uid,
-                            productId: subReq.sku_id,
-                            packageName: subReq.package_name
-                        });
-                        await updatePremiumState(subReq.uid)
-                        await adminOrderRef.update({uid: subReq.uid})
+                        await updatePurchaseInfo(
+                            firestore.doc(`${sub.uid}/account/purchases/${sub.purchase_token}`),
+                            playRes.data,
+                            sub
+                        )
+                        await updatePremiumState(sub.uid)
+                        await adminOrderRef.update({uid: sub.uid})
                         res.sendStatus(204)
                     } else {
                         res.status(409)
@@ -76,20 +138,13 @@ exports.subscribtion = functions.https.onRequest(async (req, res) => {
                     }
                 }
             } else {
-                await adminOrderRef.set(playRes.data)
-                await adminOrderRef.update({
-                    uid: subReq.uid,
-                    productId: subReq.sku_id,
-                    packageName: subReq.package_name
-                });
-                const userOrderRef = firestore.doc(`${subReq.uid}/account/purchases/${subReq.purchase_token}`);
-                await userOrderRef.set(playRes.data);
-                await userOrderRef.update({
-                    uid: subReq.uid,
-                    productId: subReq.sku_id,
-                    packageName: subReq.package_name
-                });
-                await updatePremiumState(subReq.uid);
+                await updatePurchaseInfo(adminOrderRef, playRes.data, sub)
+                await updatePurchaseInfo(
+                    firestore.doc(`${sub.uid}/account/purchases/${sub.purchase_token}`),
+                    playRes.data,
+                    sub
+                );
+                await updatePremiumState(sub.uid);
                 res.sendStatus(201)
             }
         } else {
@@ -108,17 +163,73 @@ exports.subscribtion = functions.https.onRequest(async (req, res) => {
     }
 });
 
-// @todo #2 Daily update/check the expire date.
-// Remove data if not premium
-// Disable premium if expired 30 days
-
 async function updatePremiumState(uid: string) {
     const accountRef = firestore.doc(`${uid}/account`);
-    const premium = await accountRef.collection("purchases").where("productId", "==", "premium").get();
-    // @todo #1 Check for expired date
+    const premium = await accountRef.collection("purchases")
+        .where("productId", "==", "premium")
+        .where("expiryTimeMillis", ">", new Date().getTime())
+        .get();
     if (premium.size > 0) {
         await accountRef.set({premium: true})
     } else {
         await accountRef.set({premium: false})
     }
+}
+
+async function updatePurchaseInfo(
+    userOrderRef: FirebaseFirestore.DocumentReference,
+    data: androidpublisher_v3.Schema$SubscriptionPurchase,
+    sub: Subscription
+) {
+    await userOrderRef.set(data);
+    await userOrderRef.update({
+        expiryTimeMillis: Number(data.expiryTimeMillis)
+    })
+    await userOrderRef.update({
+        uid: sub.uid,
+        productId: sub.sku_id,
+        packageName: sub.package_name
+    });
+}
+
+
+function deleteCollection(
+    db: FirebaseFirestore.Firestore,
+    collectionPath: string,
+    batchSize: number
+) {
+    let collectionRef = db.collection(collectionPath);
+    let query = collectionRef.orderBy('__name__').limit(batchSize);
+
+    return new Promise((resolve, reject) => {
+        deleteQueryBatch(db, query, resolve, reject);
+    });
+}
+
+function deleteQueryBatch(
+    db: FirebaseFirestore.Firestore,
+    query: FirebaseFirestore.Query,
+    resolve: any,
+    reject: any
+) {
+    query.get().then((snapshot) => {
+        if (snapshot.size === 0) {
+            return 0;
+        }
+        let batch = db.batch();
+        snapshot.docs.forEach((doc) => {
+            batch.delete(doc.ref);
+        });
+        return batch.commit().then(() => {
+            return snapshot.size;
+        });
+    }).then((numDeleted) => {
+        if (numDeleted === 0) {
+            resolve();
+            return;
+        }
+        process.nextTick(() => {
+            deleteQueryBatch(db, query, resolve, reject);
+        });
+    }).catch(reject);
 }
